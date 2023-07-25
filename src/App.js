726333c6d53bbf70b503a400e77bf7566b316a5b
Daud Abdul Rehman <daud.arehman@gmail.com>
2023-07-25 15:39:29 +0500

    week2day2 converting html into a component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Increment from './Component/Increment';
+import ToDoList from './Component/ToDoList'
 
 function App() {
   return (
     <div className="App">
      <Increment/>
-     <Increment/>
+     <ToDoList/>
     </div>
   );
 }
